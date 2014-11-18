9b1f9fc17c615c539ee63fb30e5350f41a8212b7
Denis Licau <denisl.pericol@gmail.com>
2014-11-18 13:46:35 +0100

    Update Program.cs

--- ForGITHUB/Program.cs
+++ ForGITHUB/Program.cs
@@ -11,6 +11,8 @@ namespace ForGITHUB
         static void Main(string[] args)
         {
             Console.WriteLine("Denis");
+            //////////This is a test/////////////
+            //Just wanted to see how GitHub works with Visual Studio 2013////////
         }
     }
 }
